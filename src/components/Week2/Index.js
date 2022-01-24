1e2a7764cbf74949723f0327c0bdff5385bd32f5
xspize <4PEREJ88@solent.ac.uk>
2022-01-24 06:25:58 +0000

    test

---
+++ src/components/Week2/Index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Pumpking from "./assets/loading_pumpkin.gif";
+import Ghost from "./assets/ghost.gif";
+import styled from "styled-components";
+
+
+const OuterWrapper = styled.div`
+background: #000000;
+width: 100vw;
+height: 100vh;
+`;
+
+const StyledWrapper = styled.div`
+    display: flex;
+    background: #000000;
+    justify-content: center;
+    height: 70vw;
+    flex-direction row-reverse
+
+`;
+
+const StyledHeader = styled.h1`
+ text-align: center;
+ color: #F7962E;
+ font-size: 29px;
+ font-family: Roboto; 
+`;
+
+const Week2 = () => (  
+ <OuterWrapper>
+    <StyledWrapper>
+        <div>
+            <img src={Ghost} />
+            <StyledHeader>Loading trick or treats</StyledHeader>
+            <img src={Pumpking} />
+        </div>
+    </StyledWrapper>
+ </OuterWrapper>
+);
+
+export default Week2;
