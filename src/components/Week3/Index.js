bdb4e8ffe411e504c2f5a6ea115265b4da433598
xspize <4PEREJ88@solent.ac.uk>
2022-01-24 08:22:08 +0000

    updated week 3

--- src/components/Week3/Index.js
+++ src/components/Week3/Index.js
@@ -1,9 +1,39 @@
+import React from "react";
+import styled from "styled-components";
 
+const StyledWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
-const Week3 = () => (  
-        <div>
-            Week 3 
-        </div>
-);
+const StyledButton = styled.button`
+    color: #fff;
+    background-color: ${(props) => props.color};
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+    border: 0;
+    border-radius: 0.25rem;
+    box-shadow: 0 0 0.5rem rgba(0,0,0.3);
+    cursor: pointer;
+    margin: 2%;
+
+`;
+
+const Button = (props) => {
+
+    return <StyledButton color= "#6200ee"> Click Me </StyledButton>
+}
+
+const Week3 = () => {  
+    return (
+    <StyledWrapper>
+
+        <Button />
+
+    </StyledWrapper>)
+};
 
 export default Week3;
