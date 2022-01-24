a227758ad0f34122ebc5b70973d4934f1215856e
xspize <4PEREJ88@solent.ac.uk>
2022-01-24 08:36:09 +0000

    commit on main added Week 4 Login Screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Week1 from "./components/Week1/Index";
 import Week2 from "./components/Week2/Index";
 import Week3 from "./components/Week3/Index";
+import Week4 from "./components/Week4/Index";
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,19 +11,20 @@ import {
   } from "react-router-dom";
 
 const Home = () => (
+    <center>
     <nav>
     <h1>Welcome to Interaction Design!</h1>
   <ul>
-    <li>
       <Link to="/Week-1">Week 1 | Loading Screen</Link>
       <br></br>
       <Link to="/Week-2">Week 2 | Notification</Link>
       <br></br>
       <Link to="/Week-3">Week 3 | Buttons </Link>
-    </li>
+      <br></br>
+      <Link to="/Week-4">Week 4 | Login Screen </Link>
   </ul>
 </nav>
-
+</center>
 );
 
 
@@ -38,6 +40,9 @@ function App(){
                 </Route>
                 <Route path="/Week-3">
                     <Week3 />
+                </Route>
+                <Route path="/Week-4">
+                    <Week4 />
                 </Route>
                 <Route path="/">
                     <Home />
---
+++ src/components/Week4/Index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Week4 = () => (
+
+<h1>Hello</h1>
+);
+
+export default Week4;
