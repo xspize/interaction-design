9837b50f5c04db171d2ac6c17dba27814874e52c
xspize <4PEREJ88@solent.ac.uk>
2022-01-26 01:03:34 +0000

    commit on week 4

--- src/components/Week4/Index.js
+++ src/components/Week4/Index.js
@@ -3,26 +3,54 @@ import styled from "styled-components";
 import orange from "./assets/orange_icon.png";
 import hideIcon from "./assets/hide_icon.svg";
 import showIcon from "./assets/show_icon.svg";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-const schema = yup.object().shape({
-    email: yup.string().required("Email is required").email("Not a valid email"),
-    password: yup.string().required("Password is required"),
-  });
+import eyetrack from "./assets/eye_track.png";
 
 const StyledWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    height: 100vh;
+    box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+    position: relative;
+    width: 428px;
+    height: 726px;
+    background: #FFFFFF;
+    margin: 0rem 0rem 0rem 66.5rem;
+    margin-top: 15rem;
+    border-radius: 10px;
 `;
 
-const StyledHeader = styled.h1`
-    color: #1e57f1;
-    margin-left: 2rem;
+const StyledNewUser= styled.button`
+    border: 0;
+    cursor: pointer;
+    background: #ffffff;
+    text-align: center;
+    margin: 0rem 0rem 0rem 19rem;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 16px;
+    color: #E78B49
+
+`;
+
+const StyledEmail = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    color: #E78B49
+
+`;
+
+const StyledPassword = styled.p`
+    font-family: Roboto;
+    margin: -3rem 0rem 0rem 0rem;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    color: #E78B49
 `;
 
 const StyledInput = styled.input`
@@ -35,44 +63,33 @@ const StyledInput = styled.input`
 `;
 
 const StyledIcon = styled.img`
-  width: 24px;
-  height: 24px;
-  margin-left: -80px;
-  margin-bottom: -10px;
-`;
-
-const StyledErrorText = styled.p`
-  color: red;
+    margin: -10rem 0rem 0rem 19rem;
+    width: 24px;
+    height: 24px;
+    margin-left: -30px;
+    margin-bottom: 50px;
 `;
 
 const StyledButton = styled.button`
-  height: 63px;
-  width: 350px;
-  background: #1e57f1;
-  color: #ffff;
-  text-align: center;
-  font-size: 18px;
-  border: 0;
-  cursor: pointer;
-  &:hover {
-    background: red;
+    height: 49px;
+    width: 231px;
+    background: #E78B49;
+    border-radius: 4px;
+    color: #ffff;
+    text-align: center;
+    font-size: 18px;
+    border: 0;
+    cursor: pointer;
+    &:hover {
+        background: #EDECEC;
   }
+
 `;
 
 
 const Week4 = () => {
 
     const [showPassword, setShowPassword] = useState(false);
-    const [submitted, setSubmitted] = useState(false);
-  
-    const { register, handleSubmit, errors } = useForm({
-      resolver: yupResolver(schema),
-    });
-    const onSubmit = (data) => {
-      setSubmitted(true);
-      console.log(data);
-    }
-  
 
     const handleClick = (e) => {
       setShowPassword(!showPassword);
@@ -80,39 +97,38 @@ const Week4 = () => {
   
     return (
       <StyledWrapper>
+        <StyledNewUser> New user? </StyledNewUser>
         <img src={orange} />
-        <StyledHeader> Sally </StyledHeader>
-        {submitted && <h1> You're all done</h1>}
-        {!submitted && (
+        <br />
+
         
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form>
           <p>
+          <StyledEmail>Email/Username</StyledEmail>
             <StyledInput
-              type="text"
-              name="email"
-              placeholder="Email"
-              ref={register()}
-            />
+            type="text" name="Email" placeholder="Email" required
+             />
+
           </p>
-          <StyledErrorText> {errors.email?.message}</StyledErrorText>
-  
           <p>
+          <StyledEmail>Password</StyledEmail>
             <StyledInput
-              type={showPassword ? "text" : "password"}
-              name="password"
-              placeholder="Password"
-              ref={register()}
+              type={showPassword ? "text" : "password"} name="password" placeholder="Password" required
             />
             <StyledIcon
               src={showPassword ? hideIcon : showIcon}
               onClick={handleClick}
             ></StyledIcon>
-            <StyledErrorText> {errors.password?.message}</StyledErrorText>
           </p>
           <p>
-            <StyledButton> Login </StyledButton>
-          </p>
-        </form>)}
+        <br />
+            <center><StyledButton> Sign in </StyledButton></center>
+            </p>
+        <br/>
+        <br/>
+        <br/>
+         <center> <img src={eyetrack} /> </center>
+        </form>
       </StyledWrapper>
     );
 };
